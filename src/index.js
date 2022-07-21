f6a9c9691796d1e7b4b0596033f1d050d240b129
Harry-Gupenec <Spektar001@mail.ru>
2022-07-21 14:11:54 +0300

    feat: add code to implement a function Morse-decoder

--- src/index.js
+++ src/index.js
@@ -38,9 +38,36 @@ const MORSE_TABLE = {
 };
 
 function decode(expr) {
-    // write your solution here
+
+  const size = 10;
+  const chapt = 2;
+  let str = '';
+
+  for (let i = 0; i < expr.length; i += size) {
+    let letter = expr.slice(i, i + size);
+    let morse = '';
+
+    if (letter === '**********') {
+      str += ' ';
+    };
+    for(let j = 0; j < letter.length; j += chapt){
+      let symbol = letter.slice(j, j + chapt);
+				if (symbol == '10') {
+					morse += '.';
+				} else if (symbol == '11') {
+					morse += '-';
+				}
+    }
+    for (let item in MORSE_TABLE) {
+      if (item === morse) {
+        str += MORSE_TABLE[item];
+      }
+    }
+  }
+
+  return str;
 }
 
 module.exports = {
-    decode
+  decode
 }
